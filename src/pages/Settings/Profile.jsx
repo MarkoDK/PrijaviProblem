45ca474adc17dd9591a7fd15eef0b67254e42718
Marko Veljkovic <markoveljkovic10110@gmail.com>
2025-06-22 15:14:49 +0200

    Final publish test 5

--- src/pages/Settings/Profile.jsx
+++ src/pages/Settings/Profile.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react';
 
 const Profile = () => {
   const [state, setState] = useState(null);
-  return <p>This is the Profile pagqe</p>;
+  return <p>Ovo je profil stranica</p>;
 };
 
 export default Profile;

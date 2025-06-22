49b7bdc5ee435df4ccef9f7cd60fed6b39142212
Marko Veljkovic <markoveljkovic10110@gmail.com>
2025-06-22 14:54:06 +0200

    Test

--- src/pages/Settings/Profile.jsx
+++ src/pages/Settings/Profile.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react';
 
 const Profile = () => {
   const [state, setState] = useState(null);
-  return <p>This is the Profile page</p>;
+  return <p>This is the Profile pagqe</p>;
 };
 
 export default Profile;
